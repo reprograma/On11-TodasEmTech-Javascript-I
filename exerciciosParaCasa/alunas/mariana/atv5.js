f5426df23974c4157ef5afc85c92ec9ed7f9d7a5
Mariana Lima <marilimabrito13@gmail.com>
2021-03-18 19:52:35 -0300

    Adicionando as atividades de casa feitas na aula 2

---
+++ exerciciosParaCasa/alunas/mariana/atv5.js
@@ -0,0 +1,9 @@
+/* - 5 *Receba do usuários 3 números ou crie variaveis com 3 números, e calcule a média entre eles.*/
+
+let num1 = 3
+let num2 = 6
+let num3=  5
+let media = (num1+ num2 + num3) / 3;
+
+console.log ('A média dos números digitados é: ', media)
+
---
+++ exerciciosParaCasa/alunas/mariana/atv7.js
@@ -0,0 +1,13 @@
+ /*Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se os números forem iguais, mostre uma mensagem no console "Os números são iguais".*/
+
+ let number1 = 6
+ let number2 = 6
+
+
+
+if (number1 > number2){
+    console.log("O número " + number1 + " é o maior" )
+} else if (number1 < number2) {
+    console.log("O número " + number2 + " é o maior" )
+} else  {
+    console.log("Os números são iguais")}
