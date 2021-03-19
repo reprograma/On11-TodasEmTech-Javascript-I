a92bbbcd5e37be286d1610b4da4fa18627c1e22a
aanacarolina <aanacarolina@gmail.com>
2021-03-19 20:09:17 -0300

    Exercicios todos Carol SN Semana 2

---
+++ exerciciosParaCasa/alunas/carolSilva/7exercicioSete.js
@@ -0,0 +1,14 @@
+/* 7 *Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, 
+se os números forem iguais, mostre uma mensagem no console "Os números são iguais".*/
+
+let a
+a = 50
+
+let b 
+b = 100
+
+b => a
+
+if(a === b) {
+    console.log (`Os numeros são iguais`)
+}
