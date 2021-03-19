61f6d0b56a383bc64123defb4607300150101384
CrysMello <crystianemello@hotmail.com>
2021-03-19 17:03:15 -0300

    Resolução dos desafios propostos

---
+++ exerciciosParaCasa/alunas/crystianeCaldeira/calculoMedia.js
@@ -0,0 +1,9 @@
+/*Receba do usuários 3 números ou crie variaveis com 3 números,
+e calcule a média entre eles*/
+
+let user1= 8.5;
+let user2= 10;
+let user3= 3.5;
+
+let calcMedia=((user1 + user2 + user3)/3);
+console.log(" Sua média é >>> "+ calcMedia)
---
+++ exerciciosParaCasa/alunas/crystianeCaldeira/diasDaSemana.js
@@ -0,0 +1,15 @@
+/*Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.*/
+
+
+let dia1="Domingo.";
+let dia2="Segunda-Feira.";
+let dia3="Terça-Feira.";
+let dia4="Quarta-feira.";
+let dia5="Quinta-feira.";
+let dia6="Sexta-feira.";
+let dia7="Sabádo.";
+
+let qualDia= dia2
+
+console.log("Olá hoje é " + qualDia);
+console.log("Mas amanhã há de ser outro dia....");
