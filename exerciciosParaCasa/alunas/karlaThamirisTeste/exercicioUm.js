2e18569eb0572f19fb5c9429a95c4e825bb35a81
Karla Thamiris <karlatn@outlook.com>
2021-03-20 20:26:17 -0300

    atividade semana 2 reprograma

---
+++ exerciciosParaCasa/alunas/karlaThamirisTeste/exercicioUm.js
@@ -0,0 +1,5 @@
+let meuNome
+meuNome = 'Karla Thamiris'
+let minhaIdade = 25
+meuNome = 'Mariana'
+console.log(minhaIdade,', ', meuNome)
---
+++ variaveisEConsole/index.js
@@ -0,0 +1,14 @@
+const nome = 'Karla Thamiris'
+console.log('O nome da pessoa é: ', nome)
+
+var idade = 26
+idade = idade + 1
+console.log('A idade da pessoa no ano que vem será: ', idade)
+
+maravilhosa = 'feijoada vegana'
+console.log(maravilhosa, typeof(maravilhosa))
+maravilhosa = true
+
+console.log(`A ${nome} de idade ${idade} é maravilhosa? ${maravilhosa}`)
+
+//O comando Alt+z faz com que todo o conteúdo da linha, por maior que seja, fiquem em exibição
