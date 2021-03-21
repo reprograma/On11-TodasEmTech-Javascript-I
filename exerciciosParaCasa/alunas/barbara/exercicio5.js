a75ca95b16d2f620a84a3b2f97dfd9f4733cf593
Babi Borges <barborges@outlook.com>
2021-03-21 00:42:36 -0300

    exercicio js

---
+++ exerciciosParaCasa/alunas/barbara/exercicio5.js
@@ -0,0 +1,7 @@
+var babi = 10
+var rafa = 15
+var fazendinha = 5
+
+var média = (babi + rafa + fazendinha) / 3;
+
+console.log(`A média entre os números ${babi}, ${rafa} e ${fazendinha}, é ${média}`);
